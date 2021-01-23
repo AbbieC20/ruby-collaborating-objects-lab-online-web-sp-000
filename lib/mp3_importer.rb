15a4df15bb0ad302d0cc11a83db3fcc4ddb7b957
remote-torrent-6747 <abbyc@sky.com>
2021-01-23 12:25:39 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+#parse all the filenames in the spec/fixtures folder
+#and send the filenames to the Song class
+
+class MP3Importer
+
+  def initialize
+  end
+
+  def filenames
+  end
+
+  def import
+  end
+  
+end
